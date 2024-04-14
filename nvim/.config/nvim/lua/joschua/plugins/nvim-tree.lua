c3eede6efb7d95b8490c4b76e9a45473f2f6c043
Joschua <70809675+selfire1@users.noreply.github.com>
2024-04-15 09:58:56 +1000

    update nvim-tree config

--- nvim/.config/nvim/lua/joschua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/joschua/plugins/nvim-tree.lua
@@ -3,31 +3,35 @@ return {
 	"nvim-tree/nvim-tree.lua",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-    local function my_on_attach(bufnr)
-  local api = require "nvim-tree.api"
+		local keymap = vim.keymap
 
-  local function opts(desc)
-    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
-
-  -- default mappings
-  api.config.mappings.default_on_attach(bufnr)
-
-  -- custom mappings
-	vim.keymap.set("n", " ", api.node.open.edit, opts("Open"))
-end
+		-- set custom mappings (without leader waiting for second argument)
+		local function my_on_attach(bufnr)
+			local api = require("nvim-tree.api")
+			local function opts(desc)
+				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+			end
+			-- default mappings
+			api.config.mappings.default_on_attach(bufnr)
+			-- custom mappings
+			vim.keymap.set("n", " ", api.node.open.edit, opts("Open"))
+		end
 
 		local nvimtree = require("nvim-tree")
 
 		-- recommended settings from nvim-tree documentation
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
+
 		nvimtree.setup({
-			-- change folder arrow icons
+			view = {
+				width = 35,
+				relativenumber = true,
+			},
 			filters = {
 				dotfiles = false,
 			},
-  on_attach = my_on_attach,
+			on_attach = my_on_attach,
 			renderer = {
 				icons = {
 					glyphs = {
@@ -38,9 +42,7 @@ end
 					},
 				},
 			},
-			-- disable window_picker for
-			-- explorer to work well with
-			-- window splits
+			-- disable window_picker for explorer to work well with window splits
 			actions = {
 				open_file = {
 					window_picker = {
@@ -48,12 +50,8 @@ end
 					},
 				},
 			},
-			-- 	git = {
-			-- 		ignore = false,
-			-- 	},
 		})
-		local keymap = vim.keymap
-		keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
-		keymap.set("n", "<leader>fe", ":NvimTreeFindFile<CR>") -- open file in explorer
+		keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+		keymap.set("n", "<leader>fe", ":NvimTreeFindFile<CR>", { desc = "Find file in file explorer" })
 	end,
 }
