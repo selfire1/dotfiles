c0fc72d9ae93229f7db6f20fc4d231f3d14ee986
Joschua <70809675+selfire1@users.noreply.github.com>
2024-04-15 09:59:25 +1000

    install plenary

--- nvim/.config/nvim/lua/joschua/plugins/init.lua
+++ nvim/.config/nvim/lua/joschua/plugins/init.lua
@@ -1,5 +1,5 @@
 return {
-	-- "windwp/nvim-ts-autotag",
+	"nvim-lua/plenary.nvim",
 	"digitaltoad/vim-pug",
 	"inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
 	"tpope/vim-surround", -- add, delete, change surroundings
