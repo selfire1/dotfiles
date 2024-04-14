4e06e4116f719b96493420a6bce5287e9a1d2598
Joschua <70809675+selfire1@users.noreply.github.com>
2024-04-15 09:59:12 +1000

    update comment

--- nvim/.config/nvim/lua/joschua/plugins/init.lua
+++ nvim/.config/nvim/lua/joschua/plugins/init.lua
@@ -2,5 +2,5 @@ return {
 	-- "windwp/nvim-ts-autotag",
 	"digitaltoad/vim-pug",
 	"inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
-	"tpope/vim-surround", -- add, delete, change surroundings (it's awesome)
+	"tpope/vim-surround", -- add, delete, change surroundings
 }
