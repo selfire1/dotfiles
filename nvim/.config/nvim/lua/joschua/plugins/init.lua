3daf4886c93606fff8f42ff151739e0b48c3bbdd
Joschua <70809675+selfire1@users.noreply.github.com>
2024-04-15 10:50:49 +1000

    replace `vim-ReplaceWithRegister` with `substitute`

--- nvim/.config/nvim/lua/joschua/plugins/init.lua
+++ nvim/.config/nvim/lua/joschua/plugins/init.lua
@@ -1,6 +1,5 @@
 return {
 	"nvim-lua/plenary.nvim",
 	"digitaltoad/vim-pug",
-	"inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
 	"tpope/vim-surround", -- add, delete, change surroundings
 }
