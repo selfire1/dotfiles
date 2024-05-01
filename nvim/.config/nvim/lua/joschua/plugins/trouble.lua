469be5306c3da32a927824006ae0ca5984b66ce1
Joschua <70809675+selfire1@users.noreply.github.com>
2024-05-01 11:46:39 +1000

    add next and previous keybind for trouble

--- nvim/.config/nvim/lua/joschua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/joschua/plugins/trouble.lua
@@ -1,6 +1,22 @@
 return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+	config = function()
+		local trouble = require("trouble")
+		local opts = {
+			skip_groups = true,
+			jump = true,
+		}
+
+		vim.keymap.set("n", "<leader>xj", function()
+			trouble.next(opts)
+		end, { desc = "Next trouble item" })
+
+		vim.keymap.set("n", "<leader>xk", function()
+			trouble.previous(opts)
+		end, { desc = "Previous trouble item" })
+	end,
+
 	keys = {
 		{ "<leader>xx", "<cmd>TroubleToggle<CR>", desc = "Open/close trouble list" },
 		{ "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
