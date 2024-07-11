8a106635bdc18be15a30dba0b54314b8f1cd3c63
Joschua <70809675+selfire1@users.noreply.github.com>
2024-07-11 10:53:49 +1000

    open git window if in shadowvim

--- nvim/.config/nvim/lua/joschua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/joschua/plugins/fugitive.lua
@@ -5,7 +5,11 @@ return {
 		-- Git keybinds / fugitive keybinds
 		keymap.set("n", "<leader>g", ":G<CR>") -- open Git view
 		keymap.set("n", "<leader>gsc", ":Gwrite | :G commit<CR>") -- git stage and commit
-		keymap.set("n", "<leader>gg", ":Gwrite | :G commit<CR>") -- git stage and commit
+		if vim.g.shadowvim then
+			keymap.set("n", "<leader>gg", "<Cmd>SVPress <LT>D-A-c><CR>") -- git stage and commit
+		else
+			keymap.set("n", "<leader>gg", ":Gwrite | :G commit<CR>") -- git stage and commit
+		end
 		keymap.set("n", "<leader>gs", ":Gwrite <CR>") -- git stage
 		keymap.set("n", "<leader>ga", ":G add --all -- ':!src/content/*' ':!.gitignore' | :G commit<CR>") -- git stage all (expect commonly ignored)
 		keymap.set("n", "<leader>gc", ":G commit<CR>") -- git commit
