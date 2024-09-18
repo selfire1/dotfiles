1a1a01c270badc6f3ad202814d3796319a600666
Joschua <70809675+selfire1@users.noreply.github.com>
2024-09-19 09:19:14 +1000

    set more subtle folded style

--- nvim/.config/nvim/lua/joschua/plugins/colourscheme.lua
+++ nvim/.config/nvim/lua/joschua/plugins/colourscheme.lua
@@ -3,7 +3,8 @@ return {
 	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
 		-- load the colorscheme here
-		vim.cmd([[colorscheme zenbones]])
+		vim.cmd("colorscheme zenbones")
+		vim.cmd("highlight Folded guibg=NONE guifg=#6E6763")
 	end,
 	dependencies = {
 		"rktjmp/lush.nvim",
