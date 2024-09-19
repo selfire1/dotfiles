d8415a3f3e1da57266c63ee9debe682ba2bbafdc
Joschua <70809675+selfire1@users.noreply.github.com>
2024-09-19 10:14:33 +1000

    set up wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,13 +1,15 @@
+-- Pull in the wezterm API
 local wezterm = require("wezterm")
-local config = {}
 
--- In newer versions of wezterm, use the config_builder which will help provide clearer error messages
-if wezterm.config_builder then
-	config = wezterm.config_builder()
+local function get_appearance()
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
 end
 
--- https://github.com/evantravers/dotfiles/blob/master/wezterm/.wezterm.lua
-function Scheme_for_appearance(appearance)
+-- Set colour scheme based on appearance
+local function scheme_for_appearance(appearance)
 	if appearance:find("Dark") then
 		return "zenbones_dark"
 	else
@@ -15,37 +17,35 @@ function Scheme_for_appearance(appearance)
 	end
 end
 
-config = {
-	color_scheme = Scheme_for_appearance(wezterm.gui.get_appearance()),
+return {
+	-- fonts
 	font = wezterm.font_with_fallback({
 		{
-			family = "Monaspace Neon Var",
-			weight = 900,
+			family = "JetBrains Mono",
+			weight = 700,
 			harfbuzz_features = {
 				"calt",
 				"liga",
 				"dlig",
 				"ss01",
-				"ss02",
-				"ss03",
-				"ss04",
-				"ss05",
-				"ss06",
-				"ss07",
-				"ss08",
 			},
 		},
 	}),
-	font_size = 16.0,
-	hide_tab_bar_if_only_one_tab = true,
+	font_size = 13.0,
+	line_height = 1.02,
+	-- theme
+	color_scheme = scheme_for_appearance(get_appearance()),
+	-- window and UI
+	window_padding = {
+		left = "2cell",
+		right = "2cell",
+		top = "1cell",
+		bottom = "1cell",
+	},
 	window_decorations = "RESIZE",
-	adjust_window_size_when_changing_font_size = false,
-	line_height = 1.1,
+	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
+	default_cursor_style = "BlinkingUnderline",
+	-- term
 	term = "wezterm",
-	send_composed_key_when_left_alt_is_pressed = false,
-	send_composed_key_when_right_alt_is_pressed = false,
-	window_close_confirmation = "NeverPrompt",
 }
-
--- and finally, return the configuration to wezterm
-return config
