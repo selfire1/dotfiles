050e60e06cebdda231caac3540353af185efeb8d
Joschua <70809675+selfire1@users.noreply.github.com>
2024-09-20 12:29:40 +1000

    only show and jump to warnings and errors

--- nvim/.config/nvim/lua/joschua/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/joschua/plugins/lsp/lspconfig.lua
@@ -29,6 +29,23 @@ return {
 
 		local keymap = vim.keymap -- for conciseness
 
+		local diagnostic_opts = {
+			float = { border = "double" },
+			-- Show gutter sings
+			signs = {
+				-- With highest priority
+				priority = 9999,
+				-- Only for warnings and errors
+				severity = { min = "WARN", max = "ERROR" },
+			},
+			-- Show virtual text only for errors
+			virtual_text = { severity = { min = "ERROR", max = "ERROR" } },
+			-- Don't update diagnostics when typing
+			update_in_insert = false,
+		}
+
+		vim.diagnostic.config(diagnostic_opts)
+
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 			callback = function(ev)
@@ -65,10 +82,18 @@ return {
 				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
 				opts.desc = "Go to previous diagnostic"
-				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+				keymap.set("n", "[d", function()
+					vim.diagnostic.goto_prev({
+						severity = { min = vim.diagnostic.severity.WARN },
+					})
+				end, { noremap = true, silent = true })
 
 				opts.desc = "Go to next diagnostic"
-				keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+				keymap.set("n", "]d", function()
+					vim.diagnostic.goto_next({
+						severity = { min = vim.diagnostic.severity.WARN },
+					})
+				end, { noremap = true, silent = true })
 
 				opts.desc = "Show documentation for what is under cursor"
 				keymap.set("n", "<leader>h", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
