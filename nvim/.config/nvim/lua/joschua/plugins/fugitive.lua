96da73100e3856bd803f14cbd32c4fd2758bc3f7
Joschua <70809675+selfire1@users.noreply.github.com>
2024-10-22 12:15:18 +1000

    clean up git commands
    reorder, remove unused ones, and add a few new ones

--- nvim/.config/nvim/lua/joschua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/joschua/plugins/fugitive.lua
@@ -5,15 +5,15 @@ return {
 		-- Git keybinds / fugitive keybinds
 		keymap.set("n", "<leader>g", ":G<CR>") -- open Git view
 		keymap.set("n", "<leader>go", ":G<CR>") -- open Git view
-		keymap.set("n", "<leader>gsc", ":Gwrite | :G commit<CR>") -- git stage and commit
-		if vim.g.shadowvim then
-			keymap.set("n", "<leader>gg", "<Cmd>SVPress <LT>D-A-c><CR>") -- git stage and commit
-		else
-			keymap.set("n", "<leader>gg", ":Gwrite | :G commit<CR>") -- git stage and commit
-		end
+
 		keymap.set("n", "<leader>gs", ":Gwrite <CR>") -- git stage
-		keymap.set("n", "<leader>ga", ":G add --all -- ':!src/content/*' ':!.gitignore' | :G commit<CR>") -- git stage all (expect commonly ignored)
 		keymap.set("n", "<leader>gc", ":G commit<CR>") -- git commit
+
+		keymap.set("n", "<leader>gsc", ":Gwrite | :G commit<CR>") -- git stage and commit
+		keymap.set("n", "<leader>gg", ":Gwrite | :G commit<CR>") -- git stage and commit
+
 		keymap.set("n", "<leader>gp", ":G push<CR>") -- git push
+		keymap.set("n", "<leader>gl", ":G log --pretty --oneline<CR>") -- git log
+		keymap.set("n", "<leader>gi", ":G rebase -i<CR>") -- git rebase
 	end,
 }
