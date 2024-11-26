e8610344adca09579ac0b81e66484ea5dd677a09
Joschua <70809675+selfire1@users.noreply.github.com>
2024-11-26 10:13:36 +1000

    Revert "use mini for pairs"
    This reverts commit 35bc992b8c0755ca8d5a779d4a9e3d446ea05315.

--- nvim/.config/nvim/lua/joschua/plugins/mini.lua
+++ nvim/.config/nvim/lua/joschua/plugins/mini.lua
@@ -3,7 +3,6 @@ return {
 	version = false, -- `main` branch (recommended)
 	config = function()
 		require("mini.jump").setup()
-		require("mini.pairs").setup()
 		require("mini.surround").setup()
 		require("mini.bracketed").setup()
 		require("mini.indentscope").setup()
