05c62007c62499e0031ce2baf67beec7f50fc0ac
Joschua <70809675+selfire1@users.noreply.github.com>
2025-01-14 16:21:51 +1000

    use prettier for vue formatting

--- nvim/.config/nvim/lua/joschua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/joschua/plugins/formatting.lua
@@ -7,6 +7,7 @@ return {
 
 		conform.setup({
 			formatters_by_ft = {
+				vue = { "prettier" },
 				javascript = { "prettier" },
 				typescript = { "prettier" },
 				javascriptreact = { "prettier" },
