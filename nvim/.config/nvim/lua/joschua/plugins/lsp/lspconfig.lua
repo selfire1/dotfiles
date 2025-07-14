f52ade481ef3cf2a0e1c0c780b2a2ac1235044fc
Joschua <70809675+selfire1@users.noreply.github.com>
2025-07-14 14:26:16 +1000

    remove comments

--- nvim/.config/nvim/lua/joschua/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/joschua/plugins/lsp/lspconfig.lua
@@ -137,13 +137,6 @@ return {
 				filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
 			},
 		})
-		-- If you are using mason.nvim, you can get the ts_plugin_path like this
-		-- For Mason v1,
-		-- local mason_registry = require('mason-registry')
-		-- local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
-		-- For Mason v2,
-		-- local vue_language_server_path = vim.fn.expand '$MASON/packages' .. '/vue-language-server' .. '/node_modules/@vue/language-server'
-		-- or even
 		local vue_language_server_path = vim.fn.stdpath("data")
 			.. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
 		local vue_plugin = {
