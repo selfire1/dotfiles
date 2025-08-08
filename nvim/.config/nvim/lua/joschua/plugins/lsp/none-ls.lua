2b5a08284cc56db2f3e11baaad9f2d888681ce14
Joschua <70809675+selfire1@users.noreply.github.com>
2025-08-08 10:29:54 +1000

    update

--- nvim/.config/nvim/lua/joschua/plugins/lsp/none-ls.lua
+++ nvim/.config/nvim/lua/joschua/plugins/lsp/none-ls.lua
@@ -22,35 +22,25 @@ return {
 			},
 		})
 
-		-- for conciseness
-		local formatting = null_ls.builtins.formatting -- to setup formatters
-		local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+		local formatting = null_ls.builtins.formatting
+		local diagnostics = null_ls.builtins.diagnostics
 
-		-- to setup format on save
 		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-		-- configure null_ls
 		null_ls.setup({
-			-- add package.json as identifier for root (for typescript monorepos)
 			root_dir = null_ls_utils.root_pattern(".null-ls-root", "Makefile", ".git", "package.json"),
-			-- setup formatters & linters
 			sources = {
-				--  to disable file types use
-				--  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
 				formatting.prettier.with({
-					extra_filetypes = { "svelte" },
-				}), -- js/ts formatter
-				formatting.stylua, -- lua formatter
+					condition = function(utils)
+						return utils.root_has_file({ ".prettierrc", ".prettierrc.cjs" })
+					end,
+				}),
+				formatting.stylua,
 				formatting.isort,
 				formatting.black,
 				diagnostics.pylint,
-				diagnostics.eslint.with({ -- js/ts linter
-					condition = function(utils)
-						return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" }) -- only enable if root has .eslintrc.js or .eslintrc.cjs
-					end,
-				}),
+				diagnostics.eslint,
 			},
-			-- configure format on save
 			on_attach = function(current_client, bufnr)
 				if current_client.supports_method("textDocument/formatting") then
 					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
